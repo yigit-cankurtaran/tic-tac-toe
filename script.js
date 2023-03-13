dcc2db60852acb53bd30877b20c80490eb06265c
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2023-03-13 17:04:59 +0300

    Simpler rewrite. Works properly, but the CSS is bad and the grid doesn't update when clicked. Will update.

---
+++ script.js
@@ -0,0 +1,53 @@
+const cells = document.querySelectorAll(".cell");
+const resetBtn = document.getElementById("reset");
+let currentPlayer = "X";
+let gameover = false;
+
+class handleCellClick {
+  constructor() {
+    if (gameover) return;
+    if (this.textContent !== "") return;
+    this.textContent = currentPlayer;
+    checkForWinner();
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+  }
+}
+
+function checkForWinner() {
+  const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8], // rows
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8], // columns
+    [0, 4, 8],
+    [2, 4, 6], // diagonals
+  ];
+
+  for (let combo of winningCombos) {
+    if (
+      cells[combo[0]].textContent === currentPlayer &&
+      cells[combo[1]].textContent === currentPlayer &&
+      cells[combo[2]].textContent === currentPlayer
+    ) {
+      alert(`Player ${currentPlayer} has won!`);
+      gameover = true;
+      return;
+    }
+  }
+
+  if (Array.from(cells).every((cell) => cell.textContent !== "")) {
+    alert("Game over. It was a tie!");
+    gameover = true;
+  }
+}
+
+function handleReset() {
+  cells.forEach((cell) => (cell.textContent = ""));
+  currentPlayer = "X";
+  gameover = false;
+}
+
+cells.forEach((cell) => cell.addEventListener("click", handleCellClick));
+resetBtn.addEventListener("click", handleReset);
